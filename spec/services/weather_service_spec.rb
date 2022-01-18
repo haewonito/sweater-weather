73d68f1c5f38e97fce7bdc0a343bff2fe72cd61a
Haewon Jeon <haewonito@gmail.com>
2022-01-18 08:46:50 -0700

    weather service rb and spec

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -1,1 +1,11 @@
+require "rails_helper"
 
+RSpec.describe "Openweather API Service" do
+    it "can get weather information", :vcr do
+      params = {lat: "39.738453", lon:"-104.984853"}
+      response = WeatherService.get_weather(params)
+      expect(response[:current]).to be_a Hash
+      expect(response[:daily]).to be_an Array
+      expect(response[:hourly]).to be_an Array
+    end
+end
